7df7eaa0a195ffe19ab1dc0d876a3c7beac30b19
Jonathan Cavalcanti <jonathan.cavalcanti11@gmail.com>
2020-06-30 20:35:52 -0300

    Adicionando nova alteracao

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// Passando uma função ao realizar um click
+document.getElementById("botaoJogar").onclick = () => { // Arrow function
+    let valor = Math.random();
+
+    let imagem = "";
+
+    if (valor > 0.5) {
+        imagem = "coroa";
+    } else {
+        imagem = "cara";
+    }
+
+    let caminhoImagem = `assets/images/${imagem}.jpg`;
+    document.getElementById("imagemMoeda").src = caminhoImagem;
+
+    console.log(imagem);
+}
+
+
+
+// let alunoA = "Jonathan";
+// let alunoB = "Maria";
+
+// // Arrays - Vetores
+// let nomeAlunos = ["Jonathan","Maria","João","Pedro"];
+
+
+// // Laço de repetição - Percorrendo o Array
+// for(let i = 0; i < 4; i++) {
+//     console.log("Nome do Aluno: " + nomeAlunos[i]);
+// }
+
+// let contador = 0;
+
+// // Estrutura de repetição - Percorrendo o Array com paradas.
+// // while (contador < 3) {
+// //     console.log("Teste");
+    
+// // }
+
+
