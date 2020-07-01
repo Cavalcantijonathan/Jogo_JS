6ed6558ff121bc1b120ea9fcc0c05f7228fea014
Jonathan Cavalcanti <jonathan.cavalcanti11@gmail.com>
2020-07-01 20:07:40 -0300

    Adicionando nome de Jogador

---
+++ aula10.js
@@ -0,0 +1,42 @@
+// //Function com paremetros.
+// function soma(numeroA, numeroB) {
+//     let total = numeroA + numeroB;
+//     return total;   // Return finaliza a funcao. 
+// } 
+
+// console.log(soma(50,30));
+
+// soma(10,15);
+// soma(10,20);
+// // Não se alterar informação ela sempre será a mesma variavel.
+//const professor = "Lucas"; => Variavel
+
+
+// let alunoA = "Jonathan"; - Voce pode alterar dentro da função.
+// let alunoB = "Maria";
+
+// // Arrays - Vetores
+// let nomeAlunos = ["Jonathan","Maria","João","Pedro"];
+
+
+// // Laço de repetição - Percorrendo o Array
+
+//for(let i = 0; i < 4; i++) {
+//    console.log("Nome do Aluno: " + nomeAlunos[i]);
+//}
+
+// Let ficar presa a função ou For(laço) ou escopo.
+// Var fica disponivel de forma global 
+
+
+// let contador = 0;
+
+// // Estrutura de repetição - Percorrendo o Array com paradas.
+// // while (contador < 3) {
+// //     console.log("Teste");
+    
+// // }
+
+// const element = (id) => console.log(id);
+
+// element("teste"); - Sintectec sugar 
--- js/app.js
+++ js/app.js
@@ -1,39 +1,41 @@
 // Passando uma função ao realizar um click
 document.getElementById("botaoJogar").onclick = () => { // Arrow function
-    let valor = Math.random();
+    const jogadorCara = getMyElement("jogadorCara").value;
+    const jogadorCoroa = getMyElement("jogadorCoroa").value;
+
+    if (jogadorCara == "" || jogadorCoroa == "") {
+        return alert("Digite os nomes");
+    }
+    
+    const valor = Math.random();
 
     let imagem = "";
+    let nomeVencedor = "";
 
     if (valor > 0.5) {
         imagem = "coroa";
+        nomeVencedor = jogadorCoroa;
     } else {
         imagem = "cara";
+        nomeVencedor = jogadorCara;
     }
 
-    let caminhoImagem = `assets/images/${imagem}.jpg`;
-    document.getElementById("imagemMoeda").src = caminhoImagem;    
+    const caminhoImagem = `assets/images/${imagem}.jpg`;
+    getMyElement("imagemMoeda").src = caminhoImagem;    
+    alert(nomeVencedor);
+}
+
+
+function getMyElement(id) {
+    return document.getElementById(id);
 }
 
 
 
-// let alunoA = "Jonathan";
-// let alunoB = "Maria";
-
-// // Arrays - Vetores
-// let nomeAlunos = ["Jonathan","Maria","João","Pedro"];
 
 
-// // Laço de repetição - Percorrendo o Array
-// for(let i = 0; i < 4; i++) {
-//     console.log("Nome do Aluno: " + nomeAlunos[i]);
-// }
 
-// let contador = 0;
 
-// // Estrutura de repetição - Percorrendo o Array com paradas.
-// // while (contador < 3) {
-// //     console.log("Teste");
-    
-// // }
+
 
 
